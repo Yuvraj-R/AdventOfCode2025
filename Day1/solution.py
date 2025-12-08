14cf8c66d22cd082f168c2d0817e6202e43e2673
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-12-08 04:12:09 -0500

    Optimize Part 1

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,13 +1,18 @@
-with open("rotations.txt", "r") as file:
-    rotations = [(l[0], int(l[1:])) for l in file.read().split()]
+def Solution():
+    dial, password = 50, 0
 
-dial, password = 50, 0
-for dr, rot in rotations:
-    if dr == 'L':
-        rot *= -1
-    dial = (dial + rot) % 100
-    if dial == 0:
-        password += 1
+    with open("rotations.txt", "r") as file:
+        for line in file:
+            direction, rotation = line[0], int(line[1:])
+            if direction == 'L':
+                rotation = -rotation
+
+            dial = (dial + rotation) % 100
+
+            if dial == 0:
+                password += 1
+
+    print(password)
 
 
-print(password)
+Solution()
