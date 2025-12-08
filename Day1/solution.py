d798bd206f86cbd5b37fb26462e81d3c65b14030
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-12-08 04:32:02 -0500

    Complete d1p2

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,4 +1,4 @@
-def Solution():
+def Part1():
     dial, password = 50, 0
 
     with open("rotations.txt", "r") as file:
@@ -15,4 +15,41 @@ def Solution():
     print(password)
 
 
-Solution()
+Part1()
+
+# --- PART 2 --- #
+
+
+def Part2():
+    dial, password = 50, 0
+
+    with open("rotations.txt", "r") as file:
+        for line in file:
+            direction, rotation = line[0], int(line[1:].strip())
+
+            if direction == 'L':
+                rotation = -rotation
+
+            # Large rotations can cross 0 multiple times; count every crossing.
+            if rotation > 0:
+                distance_to_zero = 100 if dial % 100 == 0 else 100 - \
+                    (dial % 100)
+                clicks = 0 if rotation < distance_to_zero else 1 + \
+                    (rotation - distance_to_zero) // 100
+            elif rotation < 0:
+                distance_to_zero = dial % 100 or 100
+                steps = -rotation
+                clicks = 0 if steps < distance_to_zero else 1 + \
+                    (steps - distance_to_zero) // 100
+            else:
+                clicks = 0
+            password += clicks
+
+            new_dial = (dial + rotation) % 100
+
+            dial = new_dial
+
+    print(password)
+
+
+Part2()
