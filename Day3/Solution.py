8f9c8463d194000fef54f74bac2148f2049bab6e
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-12-23 07:13:29 -0500

    Finish D3

--- Day3/Solution.py
+++ Day3/Solution.py
@@ -23,3 +23,33 @@ def Part1():
 
 
 Part1()
+
+
+def Part2():
+    with open("input.txt", "r") as file:
+        banks = [line.strip() for line in file]
+
+    res = 0
+    for bank in banks:
+        joltage = [0] * 12
+        bank_len = len(bank)
+
+        for i, digit_char in enumerate(bank):
+            digit = int(digit_char)
+            for j in range(12 - min(12, bank_len - i), 12):
+                joltage_digit = joltage[j]
+                if joltage_digit < digit:
+                    joltage[j] = digit
+                    for k in range(j+1, 12):
+                        joltage[k] = 0
+                    break
+
+        joltage_val = 0
+        for jolt in joltage:
+            joltage_val = (joltage_val * 10) + jolt
+        res += joltage_val
+
+    print(res)
+
+
+Part2()
