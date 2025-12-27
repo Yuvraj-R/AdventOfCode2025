a79acda41aa229cc12a2fd65dd483d07d85100f1
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-12-27 00:12:57 -0500

    Finish & Optimize D5

--- Day5/Solution.py
+++ Day5/Solution.py
@@ -1,4 +1,7 @@
+import bisect
+
 ### HELPERS ###
+
 
 def mergeIntervals(intervals):
     intervals.sort()
@@ -24,24 +27,34 @@ def Part1():
 
     merged_id_ranges = mergeIntervals(id_ranges)
 
-    fixed = []
-    for start, end in merged_id_ranges:
-        if fixed and start <= fixed[-1][1]:
-            if end > fixed[-1][1]:
-                fixed[-1][1] = end
-        else:
-            fixed.append([start, end])
-
-    import bisect
-    starts = [r[0] for r in fixed]
+    starts = [r[0] for r in merged_id_ranges]
 
     count = 0
     for x in ids:
         i = bisect.bisect_right(starts, x) - 1
-        if i >= 0 and x <= fixed[i][1]:
+        if i >= 0 and x <= merged_id_ranges[i][1]:
             count += 1
 
     print(count)
 
 
 Part1()
+
+### PART 2 ###
+
+
+def Part2():
+    with open("input.txt", "r") as file:
+        id_ranges, ids = file.read().split("\n\n")
+
+    id_ranges = [tuple(map(int, line.split('-')))
+                 for line in id_ranges.split()]
+    ids = [int(x) for x in ids.split()]
+
+    merged_id_ranges = mergeIntervals(id_ranges)
+
+    res = sum(end - start + 1 for start, end in merged_id_ranges)
+    print(res)
+
+
+Part2()
