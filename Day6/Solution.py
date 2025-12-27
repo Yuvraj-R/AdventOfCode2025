7a454e60184f4ab5e6e7e78e3afcd8ed9a90d3c5
Yuvraj Rekhi <rekhiyuvraj@gmail.com>
2025-12-27 15:50:50 -0500

    Optimize D6P1

--- Day6/Solution.py
+++ Day6/Solution.py
@@ -3,18 +3,17 @@ from math import prod
 
 def Part1():
     with open("input.txt", "r") as file:
-        lines = [line.strip().split() for line in file]
+        rows = [line.split() for line in file]
 
-    for i in range(len(lines) - 1):
-        lines[i] = [int(n) for n in lines[i]]
+    ops = rows[-1]
+    nums_rows = [list(map(int, row)) for row in rows[:-1]]
+
+    op_fn = {'+': sum, '*': prod}
 
     res = 0
-    for i, op in enumerate(lines[-1]):
-        nums = [line[i] for line in lines[:-1]]
-        if op == '+':
-            res += sum(nums)
-        else:
-            res += prod(nums)
+    for col, op in zip(zip(*nums_rows), ops):
+        res += op_fn[op](col)
+
     print(res)
 
 
